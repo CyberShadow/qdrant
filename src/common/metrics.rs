9af8de1ae77c3b435bc1ebc1285804bdd679e0a3
jojii <jojii@gmx.net>
2025-10-23 11:27:27 +0200

    Add to metrics API

--- src/common/metrics.rs
+++ src/common/metrics.rs
@@ -190,6 +190,8 @@ impl MetricsProvider for CollectionsTelemetry {
 
         let mut total_dead_shards = 0;
 
+        let mut vector_count_by_name = vec![];
+
         for collection in self.collections.iter().flatten() {
             let collection = match collection {
                 CollectionTelemetryEnum::Full(collection_telemetry) => collection_telemetry,
@@ -209,6 +211,13 @@ impl MetricsProvider for CollectionsTelemetry {
                 collection.count_vectors() as f64,
                 &[("id", &collection.id)],
             ));
+
+            for (vec_name, count) in collection.count_points_per_vector() {
+                vector_count_by_name.push(gauge(
+                    count as f64,
+                    &[("collection", &collection.id), ("vector", &vec_name)],
+                ))
+            }
 
             let points_excluded_from_index_only = collection
                 .shards
@@ -238,6 +247,15 @@ impl MetricsProvider for CollectionsTelemetry {
                 .flatten()
                 .filter(|i| i.replicate_states.values().any(|state| !state.is_active()))
                 .count();
+        }
+
+        if !vector_count_by_name.is_empty() {
+            metrics.push(metric_family(
+                "collection_name_vectors",
+                "amount of vectors grouped by vector name",
+                MetricType::GAUGE,
+                vector_count_by_name,
+            ));
         }
 
         if !indexed_only_excluded.is_empty() {
